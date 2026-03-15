71e130ca8c4b642543e05fd6c438c197a6d16747
Nicolas Do Huu <nicolas.dohuu@wheelhouse.paris>
2026-03-15 23:13:57 +0100

    chore: bump wh to 0.1.17-alpha

--- Formula/wh.rb
+++ Formula/wh.rb
@@ -1,7 +1,7 @@
 class Wh < Formula
   desc "The operating infrastructure for autonomous agent factories"
   homepage "https://docs.wheelhouse.paris"
-  version "0.1.16-alpha"
+  version "0.1.17-alpha"
   license "Apache-2.0"
 
   option "with-podman", "Install Podman container runtime (required to run agents)"
@@ -10,8 +10,8 @@ class Wh < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/Wheelhouse-Paris/wheelhouse/releases/download/v0.1.16-alpha/wh-aarch64-apple-darwin.tar.gz"
-      sha256 "30479c37befc05761ad895c577777cf9dd33d0986b8f6eb728c61ab60f7bb6af"
+      url "https://github.com/Wheelhouse-Paris/wheelhouse/releases/download/v0.1.17-alpha/wh-aarch64-apple-darwin.tar.gz"
+      sha256 "fe814507e95df6a6960cc093101d41e9f2419146503411b3f45e0b20cc91fdaa"
     end
   end
 
